41f1ac58443b7d7449fe1c6837b965d7467df7e3
MrShifu01 <stander.christian.com>
2023-08-03 09:15:23 +0200

    Final touches

--- js/script.js
+++ js/script.js
@@ -2,15 +2,24 @@
 
 function userScroll() {
     const navbar = document.querySelector('.navbar')
+    const toTopBtn = document.querySelector('#to-top')
 
     window.addEventListener('scroll', () => {
         if (window.scrollY > 50) {
             navbar.classList.add('navbar-sticky')
+            toTopBtn.classList.add('show')
         } else {
             navbar.classList.remove('navbar-sticky')
+            toTopBtn.classList.remove('show')
         }
     })
 }
+
+function scrollToTop() {
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+}
+
 
 function incrementStats () {
     const counters = document.querySelectorAll('.counter')
@@ -38,3 +47,4 @@ function incrementStats () {
 
 document.addEventListener('DOMContentLoaded', userScroll)
 document.addEventListener('DOMContentLoaded', incrementStats)
+document.querySelector('#to-top').addEventListener('click', scrollToTop)
